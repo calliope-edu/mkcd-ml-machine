e33d1fae871cb7d70e415cd79ee434e0d27aef18
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-07-25 12:19:32 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 //% color=190 weight=100 icon="\uf121" block="MLMachine"
 namespace MLMachine {
     //% block
-    export function test(): void { }
+    export function test(): void { 
+        bluetooth.onUartDataReceived("%", () => {
+            basic.showString("hello");
+        })
+    }
 }
