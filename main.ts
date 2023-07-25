8d4de10ff59ff81dab401b3bd543101a843bf828
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-07-25 15:02:48 +0200

    Added pairing pattern

--- main.ts
+++ main.ts
@@ -3,6 +3,14 @@ namespace MLMachine {
     const DELIM_SYMBOL = "#"
     let is_setup = false
 
+    const CODEBOOK: string[][] = [
+        ['t', 'a', 't', 'a', 't'],
+        ['p', 'e', 'p', 'e', 'p'],
+        ['g', 'i', 'g', 'i', 'g'],
+        ['v', 'o', 'v', 'o', 'v'],
+        ['z', 'u', 'z', 'u', 'z'],
+    ];
+
     function setup(): void {
         bluetooth.setTransmitPower(7);
         bluetooth.startUartService();
@@ -11,6 +19,46 @@ namespace MLMachine {
         bluetooth.startIOPinService();
         bluetooth.startLEDService();
     }
+
+    //% block
+    export function showPairingPattern(): void {
+        const name = control.deviceName()
+        const pattern = nameToPattern(name);
+        for (let x = 0; x < 5; x++) {
+            for (let y = 0; y < 5; y++) {
+                const inx = x + 5*y
+                const state = pattern[inx]
+                if (state) {
+                    led.plot(x, y)
+                } else {
+                    led.unplot(x,y)
+                }
+            }
+        }
+    }
+
+    function nameToPattern(name: string): boolean[] {
+        const pattern: boolean[] = [
+            true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true
+        ]
+
+        // if wrong name length, return empty pattern
+        if (name.length != 5) {
+            return pattern.map(() => false);
+        }
+
+        for (let column = 0; column < 5; column++) {
+            for (let row = 0; row < 5; row++) {
+                if (CODEBOOK[row][column] === name.charAt(column)) {
+                    break;
+                }
+                pattern[5 * row + column] = false;
+            }
+        }
+
+        return pattern;
+    }
+
 
     //% block
     export function onGestureRecognized(gesture: string = "shake", body: () => void): void {
