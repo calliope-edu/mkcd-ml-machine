430b795b4cc68005f4d90c4c0df761c2e8ff5884
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-07-25 12:15:36 +0200

    Icon changed

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-//% color=190 weight=100 icon="graduation-cap" block="MLMachine"
+//% color=190 weight=100 icon="\uf121" block="MLMachine"
 namespace MLMachine {
     //% block
     export function test(): void { }
