fcb6afe277fcf1ce1f4789999af49172af160166
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-07-25 12:09:23 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+//% color=190 weight=100 icon="\uf1ec" block="MLMachine"
+namespace MLMachine {
+    //% block
+    export function test(v: number, interval: number = 150): void { }
+}
+
 basic.forever(function () {
 	
 })
