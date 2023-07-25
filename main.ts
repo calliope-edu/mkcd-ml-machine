94734e232087c211dafebd6dfb77e5dcb3b7a6b2
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-07-25 14:51:06 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 //% color=190 weight=100 icon="\uf121" block="MLMachine"
 namespace MLMachine {
     const DELIM_SYMBOL = "#"
+    let is_setup = false
 
-    //% block
-    export function setup(): void {
+    function setup(): void {
         bluetooth.setTransmitPower(7);
         bluetooth.startUartService();
         bluetooth.startAccelerometerService();
@@ -14,6 +14,10 @@ namespace MLMachine {
 
     //% block
     export function onGestureRecognized(gesture: string = "shake", body: () => void): void {
+        if (is_setup === false) {
+            setup()
+            is_setup = true
+        }
         bluetooth.onUartDataReceived(DELIM_SYMBOL, function () {
             const input = bluetooth.uartReadUntil(DELIM_SYMBOL)
             if (input == gesture) {
