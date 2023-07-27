1dbd5ac1a473a3819f18e2cf6bd631e6c0fd644c
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-07-27 12:26:33 +0200

    Moved pairing pattern logic outside main

--- main.ts
+++ main.ts
@@ -30,14 +30,6 @@ namespace MLMachine {
         }
     }
 
-    const CODEBOOK: string[][] = [
-        ['t', 'a', 't', 'a', 't'],
-        ['p', 'e', 'p', 'e', 'p'],
-        ['g', 'i', 'g', 'i', 'g'],
-        ['v', 'o', 'v', 'o', 'v'],
-        ['z', 'u', 'z', 'u', 'z'],
-    ];
-
     function requireSetup(): void {
         if (is_setup) {
             return
@@ -56,48 +48,6 @@ namespace MLMachine {
             gestureRecognitions.fireCallbackFor(gestureName); // Could be something different from gesture names
         });
     }
-
-    //% block
-    export function showPairingPattern(): void {
-        requireSetup();
-        const name = control.deviceName()
-        const pattern = nameToPattern(name);
-        for (let x = 0; x < 5; x++) {
-            for (let y = 0; y < 5; y++) {
-                const inx = x + 5*y
-                const state = pattern[inx]
-                if (state) {
-                    led.plot(x, y)
-                } else {
-                    led.unplot(x,y)
-                }
-            }
-        }
-    }
-
-    function nameToPattern(name: string): boolean[] {
-        requireSetup();
-        const pattern: boolean[] = [
-            true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true
-        ]
-
-        // if wrong name length, return empty pattern
-        if (name.length != 5) {
-            return pattern.map(() => false);
-        }
-
-        for (let column = 0; column < 5; column++) {
-            for (let row = 0; row < 5; row++) {
-                if (CODEBOOK[row][column] === name.charAt(column)) {
-                    break;
-                }
-                pattern[5 * row + column] = false;
-            }
-        }
-
-        return pattern;
-    }
-
 
     //% block
     export function onGestureRecognized(gesture: string = "shake", body: () => void): void {
---
+++ pairingpattern.ts
@@ -0,0 +1,49 @@
+namespace MLMachine {
+    
+    const CODEBOOK: string[][] = [
+        ['t', 'a', 't', 'a', 't'],
+        ['p', 'e', 'p', 'e', 'p'],
+        ['g', 'i', 'g', 'i', 'g'],
+        ['v', 'o', 'v', 'o', 'v'],
+        ['z', 'u', 'z', 'u', 'z'],
+    ];
+
+    //% block
+    export function showPairingPattern(): void {
+        const name = control.deviceName()
+        const pattern = nameToPattern(name);
+        for (let x = 0; x < 5; x++) {
+            for (let y = 0; y < 5; y++) {
+                const inx = x + 5 * y
+                const state = pattern[inx]
+                if (state) {
+                    led.plot(x, y)
+                } else {
+                    led.unplot(x, y)
+                }
+            }
+        }
+    }
+
+    function nameToPattern(name: string): boolean[] {
+        const pattern: boolean[] = [
+            true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true
+        ]
+
+        // if wrong name length, return empty pattern
+        if (name.length != 5) {
+            return pattern.map(() => false);
+        }
+
+        for (let column = 0; column < 5; column++) {
+            for (let row = 0; row < 5; row++) {
+                if (CODEBOOK[row][column] === name.charAt(column)) {
+                    break;
+                }
+                pattern[5 * row + column] = false;
+            }
+        }
+
+        return pattern;
+    }
+}
