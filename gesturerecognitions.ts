9e78724541b31521da5656c2e0106f9c5e2aec41
A. Malthe Henriksen <andreashenriksen@yahoo.dk>
2023-08-30 16:51:13 +0200

    Update gesturerecognitions.ts

--- gesturerecognitions.ts
+++ gesturerecognitions.ts
@@ -12,12 +12,12 @@ namespace MLMachine {
         }
 
         public addRecognitionCallback(gestureName: string, callback: () => void) {
-            this.gestureNames.push(gestureName)
+            this.gestureNames.push(gestureName.toLowerCase())
             this.callbacks.push(callback)
         }
 
         public fireCallbackFor(gestureName: string) {
-            const callbackIndex = this.gestureNames.indexOf(gestureName)
+            const callbackIndex = this.gestureNames.indexOf(gestureName.toLowerCase())
             if (callbackIndex == -1) {
                 return
             }
